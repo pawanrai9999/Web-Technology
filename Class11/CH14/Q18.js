a3624e3fffb795443d579e91a3cdb5d232b33632
Pawan Kumar <pawanrai9999@gmail.com>
2017-12-07 22:17:20 +0530

    Added Q18

---
+++ Class11/CH14/Q18.js
@@ -0,0 +1,14 @@
+/*  Author : PAWAN KUMAR
+    DATE : 07 - DECEMBER - 2017
+    This program is prints n terms
+*/
+
+var nTerm = parseInt(prompt("Enter the terms: "));
+
+for(i = 1; i < n; i++){
+  document.write((i*2) + " ");
+}
+
+for(i = 1; i < n; i++){
+  document.write((i*i) + " ");
+}
