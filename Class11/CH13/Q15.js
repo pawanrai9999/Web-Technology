c11ad5c679aed57959ec0c195c72267a50b5af71
Pawan Kumar <pawanrai9999@gmail.com>
2017-12-07 20:25:06 +0530

    Q13 DONE OF CLASS 11

---
+++ Class11/CH13/Q15.js
@@ -0,0 +1,10 @@
+/* Author : PAWAN KUMAR
+  This program takes input of first, last and middle name and displays them
+  with space
+*/
+
+var firstName = prompt("Enter Your First Name: ");
+var middleName = prompt("Enter Your Middle Name: ");
+var lastName = prompt("Enter Your Last Name: ");
+
+document.write(firstName + " " + middleName + " " + lastName);
