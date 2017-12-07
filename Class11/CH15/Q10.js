7dc3fe4de6878be4e7e9b716f6712d574f804de4
Pawan Kumar <pawanrai9999@gmail.com>
2017-12-07 22:20:51 +0530

    Added CH15 and Q10

---
+++ Class11/CH15/Q10.js
@@ -0,0 +1,12 @@
+/*  Author : PAWAN KUMAR
+    DATE : 07 - DECEMBER - 2017
+    This program prints character given times
+*/
+
+function printChar(a, b){
+  for(i = 0; i < b; i++){
+    document.write(a);
+  }
+}
+
+printChar("x", 10)
