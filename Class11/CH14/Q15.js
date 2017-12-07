00945a80b985efaf2d256f15ff76174575ca5650
Pawan Kumar <pawanrai9999@gmail.com>
2017-12-07 22:07:09 +0530

    Added Q15

---
+++ Class11/CH14/Q15.js
@@ -0,0 +1,14 @@
+/*  Author : PAWAN KUMAR
+    DATE : 07 - DECEMBER - 2017
+    This program is rewritten
+*/
+
+var arr = new Array(3);
+a=2;
+k = 0;
+do{
+  t = a*a;
+  arr[k] = t;
+  a = a + 3;
+  k++
+}while(k <= 3);
